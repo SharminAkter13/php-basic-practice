3d206dd6a15c4db4a5ee434dd45f698bd9038e0e
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-18 11:37:34 +0600

    new

---
+++ task1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP </title>
+</head>
+<body>
+    <h1>My First PHP</h1>
+    <?php echo "Hello World";?>
+    <?php echo "Its Healing Now";?>
+    <?php echo "Stay Healthy & Happy";?>
+</body>
+</html>
