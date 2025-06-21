73d810a7b1f310027fa69ac65f1073e617b74a7c
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-22 00:17:04 +0600

    new

--- data-type.php
+++ data-type.php
@@ -10,15 +10,51 @@
     <h4>null</h4>
     <?php
     $v =null;
-    echo $v;
+    var_dump( $v);
     ?>
 
     <h4>boolean</h4>
     <?php
     $x =false;
     echo $x;
-    var_dump("$x")
+    var_dump($x)
     ?>
+
+    <h4>integer</h4>
+
+    <?php
+    $large_number = 50000000;
+    var_dump($large_number);      
+    ?>
+     <h4>float</h4>
+
+    <?php
+    $number = 5000.1010;
+    var_dump($number);      
+    ?>
+
+     <h4>string</h4>
+
+    <?php
+    $text = "this is string";
+    var_dump($text);      
+    ?>
+
+     <h4>numeric string</h4>
+
+    <?php
+    var_dump("0D1" == "000"); 
+    var_dump("0E1" == "000"); 
+    var_dump("2E1" == "020");
+    ?>
+
+     <h4>array</h4>
+
+    <?php
+    $array = array("fool", "bar", "hello", "world");
+    var_dump($array);
+    ?>
+        
 
 </body>
 </html>
