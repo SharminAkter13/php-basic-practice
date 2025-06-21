e011c5e3408495b1cb05b2aec793ab73d4412836
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-21 23:58:02 +0600

    new

---
+++ data-type.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php data type</title>
+</head>
+<body>
+    <h1>php data type</h1>
+    <h4>null</h4>
+    <?php
+    $v =null;
+    echo $v;
+    ?>
+
+    <h4>boolean</h4>
+    <?php
+    $x =false;
+    echo $x;
+    var_dump("$x")
+    ?>
+
+</body>
+</html>
