5124e5289517cc0260d2536389bb185584d391b3
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-26 23:21:36 +0600

    new

--- task/prime_number.php
+++ task/prime_number.php
@@ -1,20 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prime Number</title>
+    <title>Prime Number Checker</title>
 </head>
 <body>
-<form method="post" action="">
-       Enter Number:
-        <input type="number" id="num" name="num" required><br><br>
+    <h2>Check Prime Number</h2>
 
-        <input type="submit" value="Find Prime ">
+    <form method="post" action="">
+        Enter a number: <input type="number" name="num" min="1" required>
+        <input type="submit" name="submit" value="Check">
     </form>
-    <?php
-    
-    ?>
 
+    <?php
+    if (isset($_POST['submit'])) {
+        $num = $_POST['num'];
+        $isPrime = true;
+
+        if ($num <= 1) {
+            $isPrime = false;
+        } else {
+            for ($i = 2; $i <= sqrt($num); $i++) {
+                if ($num % $i == 0) {
+                    $isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if ($isPrime) {
+            echo "<h3>$num is a Prime Number ✅</h3>";
+        } else {
+            echo "<h3>$num is NOT a Prime Number ❌</h3>";
+        }
+    }
+    ?>
 </body>
 </html>
