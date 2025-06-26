619b26148e7df9c4b0d7fd6cdf7e47fce68310dd
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-26 13:04:31 +0600

    new

--- task/find_largest_no.php
+++ task/find_largest_no.php
@@ -3,45 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>find largest number</title>
+    <title>Find Largest Number</title>
 </head>
 <body>
-<form action="" method=POST>
-    Number X: <br />
-    <input
-            type="text"
-            name="fname1"
-            placeholder="please enter your name"
-          />
-          <br /><br />
-          Number Y: <br />
-          <input
-            type="text"
-            name="email1"
-            placeholder="please enter your email"
-          />
-          <br /><br />
-          Number Z: <br />
-          <input
-            type="text"
-            name="email1"
-            placeholder="please enter your email"
-          />
-          <br /><br />
-          
-</form>
+    <h3>Find the Largest of Three Numbers</h3>
+
+    <form method="post" action="">
+        Number X:
+        <input type="number" id="numX" name="numX" required><br><br>
+        Number Y:
+        <input type="number" id="numY" name="numY" required><br><br>
+        Number Z:
+        <input type="number" id="numZ" name="numZ" required><br><br>
+
+        <input type="submit" value="Find Largest">
+    </form>
+
     <?php
-    $x =3;
-    $y =4;
-    $z =5;
-    if(($x>$y)&&($x>$z)){
-        echo "The Largest Number is X: $x";
-    }elseif(($y>$x)&&($y>$z)){
-        echo "The Largest Number is Y: $y";
-    }else{
-        echo "The Largest Number is Z: $z";
+    // PHP code to process the form submission
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve the numbers from the form
+        $x = $_POST['numX'];
+        $y = $_POST['numY'];
+        $z = $_POST['numZ'];
+
+        echo "<h4>Results:</h4>";
+        echo "<p>You entered X: $x, Y: $y, Z: $z</p>";
+
+        // Logic to find the largest number
+        if (($x > $y) && ($x > $z)) {
+            echo "<p>The Largest Number is X: <strong>$x</strong></p>";
+        } elseif (($y > $x) && ($y > $z)) {
+            echo "<p>The Largest Number is Y: <strong>$y</strong></p>";
+        } else {
+            echo "<p>The Largest Number is Z: <strong>$z</strong></p>";
+        }
     }
     ?>
-    
 </body>
 </html>
---
+++ task/prime_number.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prime Number</title>
+</head>
+<body>
+<form method="post" action="">
+       Enter Number:
+        <input type="number" id="num" name="num" required><br><br>
+
+        <input type="submit" value="Find Prime ">
+    </form>
+    <?php
+    
+    ?>
+
+</body>
+</html>
