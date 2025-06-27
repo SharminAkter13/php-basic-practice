1812058ed3231c0c8f6d8d8c54d637db86cb896c
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-27 14:09:11 +0600

    new

--- task/grade_point.php
+++ task/grade_point.php
@@ -35,5 +35,43 @@
         echo "<h3>Your Grade Point is: $grade</h3>";
     }
     ?>
+
+        //switch Statement
+
+<?php
+    if (isset($_POST['submit'])) {
+        $result = $_POST['result'];
+        $grade = "";
+
+        // Convert score to grade group
+        switch (true) {
+            case ($result >= 80 && $result <= 100):
+                $grade = "5.00 (A+)";
+                break;
+            case ($result >= 70):
+                $grade = "4.00 (A)";
+                break;
+            case ($result >= 60):
+                $grade = "3.50 (A-)";
+                break;
+            case ($result >= 50):
+                $grade = "3.00 (B)";
+                break;
+            case ($result >= 40):
+                $grade = "2.00 (C)";
+                break;
+            case ($result >= 33):
+                $grade = "1.00 (D)";
+                break;
+            case ($result >= 0):
+                $grade = "0.00 (F)";
+                break;
+            default:
+                $grade = "Invalid input!";
+        }
+
+        echo "<h3>Your Grade Point is: $grade</h3>";
+    }
+    ?>
 </body>
 </html>
