1acbf20478fc33e2b2f9bfe1c43ba959716f0f70
SharminAkter13 <sharminakter13092000@gmail.com>
2025-06-28 23:10:40 +0600

    new

--- task/prime_number.php
+++ task/prime_number.php
@@ -17,7 +17,7 @@
         $num = $_POST['num'];
         $isPrime = true;
 
-        if ($num <= 1) {
+        if ($num == 1|| $num ==0) {
             $isPrime = false;
         } else {
             for ($i = 2; $i <= ($num); $i++) {
@@ -31,7 +31,7 @@
         if ($isPrime) {
             echo "<h3> $num is a Prime Number</h3>";
         } else {
-            echo "<h3> $num is NOT a Prime Number </h3>";
+            echo "<h3> $num is NOT a Prime Number or Composite</h3>";
         }
     }
     ?>
