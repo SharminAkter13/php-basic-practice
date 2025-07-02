8f4e0e0fdd40cc9bc0bedf68c1a731601ad35d9b
SharminAkter13 <sharminakter13092000@gmail.com>
2025-07-02 23:58:00 +0530

    new

---
+++ logic/leapyear.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leap Year Checker</title>
+</head>
+<body>
+    <h2>Leap Year Checker in PHP</h2>
+
+    <form method="post" action="">
+        Enter a year: <input type="number" name="year" required>
+        <input type="submit" name="submit" value="Check">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $year = $_POST['year'];
+
+        if (($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0)) {
+            echo "<h3>$year is a leap year.</h3>";
+        } else {
+            echo "<h3>$year is not a leap year.</h3>";
+        }
+    }
+    ?>
+</body>
+</html>
